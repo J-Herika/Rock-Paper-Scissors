a19132504805f120f18a1765972b8f2676804fe1
J-herika <jamalherika@gmail.com>
2024-04-28 12:34:00 +0300

    added the buttons, linked them to javascript

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ let computerChoice;
 let humanScore = 0;
 let computerScore = 0;
 
+//get access to the buttons
+const buttonRock = document.querySelector("rock");
+const buttonPaper = document.querySelector("paper");
+const buttonScissors = document.querySelector("scissors");
+
+// to get the computer choice
 function getComputerChoice() {
   let randNumber = parseInt(Math.random() * 3);
   computerChoice = options[randNumber];
   return computerChoice;
 }
+
+// to get the player choice
 function getHumanChoice() {
   let choice = prompt("Enter Rock Paper or Scissors!");
   choice.toLowerCase();
@@ -20,6 +28,8 @@ function getHumanChoice() {
     console.log("you didn't enter a valid Choice");
   }
 }
+
+// to get the result of the game round
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     console.log("its a tie!");
@@ -44,14 +54,14 @@ function playGame() {
   playRound(humanSelection, computerSelection);
   console.log(`Your score: ${humanScore} Computer Score: ${computerScore}`);
 
-  if (humanScore === 5 && computerScore != 5)
-    console.log(
-      `YOU WON the game! you:${humanScore} : computer:${computerScore}`
-    );
-  else if (humanScore != 5 && computerScore === 5)
-    console.log(
-      `you lost the game. you:${humanScore} : computer:${computerScore}`
-    );
+  // if (humanScore === 5 && computerScore != 5)
+  //   console.log(
+  //     `YOU WON the game! you:${humanScore} : computer:${computerScore}`
+  //   );
+  // else if (humanScore != 5 && computerScore === 5)
+  //   console.log(
+  //     `you lost the game. you:${humanScore} : computer:${computerScore}`
+  //   );
 }
 
 while (humanScore <= 5 || computerScore <= 5) {
